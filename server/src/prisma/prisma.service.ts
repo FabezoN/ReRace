7097f215e9e15b900b2f507b2c3959d319b50922
Fabezo <92574746+FabezoN@users.noreply.github.com>
2026-03-19 15:31:18 +0100

    fix prisma ci

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,10 +1,17 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      this.logger.log('Connexion base de donnees etablie');
+    } catch (error) {
+      this.logger.error('Echec de la connexion base de donnees au demarrage', error);
+    }
   }
 
   async onModuleDestroy() {
